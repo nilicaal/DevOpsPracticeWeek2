5420091c8711d4e21dfc56067cbfbc262a90e1d0
Niels Zwemmer <niels.zwemmer@student.uva.nl>
2021-02-15 21:47:26 +0100

    Fixed separate databases questionmark?

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -3,64 +3,53 @@ import logging
 import os
 import tempfile
 
-from tinydb import TinyDB, Query
-from tinydb.middlewares import CachingMiddleware
+import pymongo
 from functools import reduce
 import uuid
 
 from swagger_server.models import Student
 
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+client = pymongo.MongoClient('mongodb://example_db:27017/')
+db = client["studentsdb"]
+student_db = db["students"]
 
-# This comment's main purpose is also to serve as a commit-inducing
-# entity.
 def add_student(student):
     if (student.first_name is None or student.last_name is None):
         return 'unable to add user, provide all details.', 405
 
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
+    res = student_db.find({"first_name" : student.first_name,
+                         "last_name" : student.last_name })
+
+    if res.count() > 0:
         return 'already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
+    res = student_db.insert_one(student.to_dict())
     return student.student_id
 
 # Returns a student by their ID.
 def get_student_by_id(student_id, subject):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
+    student = student_db.find({"doc_id": int(student_id)})
+
+    if student.count() < 1:
+        return 'no students found', 404
+
+    student = Student.from_dict(student)
+    if not subject or subject in student.grades:
         return student
+    else:
+        return 'no students found', 404
+
+
+def get_student_by_last_name(last_name):
+    student = student_db.find({"last_name": student.last_name})
+
+    if student.count() < 1:
+        return "no students found", 404
 
     student = Student.from_dict(student)
 
-    # If no subject was provided, or if it exists
-    # in the list of subjects:
-    if not subject or subject in student.grades:
-        return student
-    else:
-        return 'Student not found', 404
+    return student, 200
 
-def get_student_by_last_name(last_name, subject):
-    student = Query()
-    student = student_db.get(student.last_name == last_name)
-
-    if not student:
-        return student
-
-    # If no subject was provided, or if it exists
-    # in the list of subjects:
-    if not subject or subject in student.grades:
-        return student
-    else:
-        return 'Student not found', 404
 
 def delete_student(student_id):
     student = student_db.get(doc_id=int(student_id))
